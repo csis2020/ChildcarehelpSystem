d69fa66ff6ed45051fdc4fcb11027128ef15ac57
csis2020 <koh11@student.douglascollege.ca>
2021-03-28 16:29:48 -0700

    first commit

---
+++ src/test/java/com/example/childcareHelp/ChildcareHelpApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.childcareHelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildcareHelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+		//test
+		//Test
+	}
+
+}
